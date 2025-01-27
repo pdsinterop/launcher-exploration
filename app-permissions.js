b0e9ec071a4e19676e095e27956a0fd3cc06c5ca
Michiel de Jong <michiel@pondersource.com>
2025-01-27 15:07:09 +0100

    window.appInstaller

--- app-permissions.js
+++ app-permissions.js
@@ -18,9 +18,20 @@ class AppInstaller {
     });
     const things = await res.json();
     const person = things.filter(x => { console.log(x); return (x['@type'].indexOf('http://xmlns.com/foaf/0.1/Person') !== -1); });
-    const privateTypeIndexes = person['http://www.w3.org/ns/solid/terms#privateTypeIndex'];
-    const publicTypeIndexes = person['http://www.w3.org/ns/solid/terms#publicTypeIndex'];
+    if (person.length !== 1) {
+      throw new Error('Unexpexted RDF in WebID Profile Document - not one Person');
+    }
+    const privateTypeIndexes = person[0]['http://www.w3.org/ns/solid/terms#privateTypeIndex'];
+    const publicTypeIndexes = person[0]['http://www.w3.org/ns/solid/terms#publicTypeIndex'];
     return privateTypeIndexes.concat(publicTypeIndexes).map(indexThing => indexThing['@id']);
   }
-  async getRegistrations(typeIndex, rdfClass) {}
+  async getRegistrations(typeIndex, rdfClass) {
+    const res = await this.fetch(this.webId, {
+      'headers': {
+        'Accept': 'application/ld+json'
+      }
+    });
+    const things = await res.json();
+    console.log(things);
+  }
 }
