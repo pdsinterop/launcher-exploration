93dad255830350d290541e3199d375f3f28c99bf
Michiel de Jong <michiel@pondersource.com>
2025-01-27 14:52:05 +0100

    Fetch typeindexes

--- app-permissions.js
+++ app-permissions.js
@@ -12,11 +12,15 @@ class AppInstaller {
   }
   async getTypeIndexes() {
     const res = await this.fetch(this.webId, {
-      // 'headers': {
-      //   'Accept': 'application/ld+json'
-      // }
+      'headers': {
+        'Accept': 'application/ld+json'
+      }
     });
-    const links = await res.json();
-    console.log(links);
+    const things = await res.json();
+    const person = things.filter(x => { console.log(x); return (x['@type'].indexOf('http://xmlns.com/foaf/0.1/Person') !== -1); });
+    const privateTypeIndexes = person['http://www.w3.org/ns/solid/terms#privateTypeIndex'];
+    const publicTypeIndexes = person['http://www.w3.org/ns/solid/terms#publicTypeIndex'];
+    return privateTypeIndexes.concat(publicTypeIndexes).map(indexThing => indexThing['@id']);
   }
+  async getRegistrations(typeIndex, rdfClass) {}
 }
