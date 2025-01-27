2cc28698863a7a3e5a6b491c79312f88da6d0aea
Michiel de Jong <michiel@pondersource.com>
2025-01-27 14:40:53 +0100

    AppInstaller

--- app-permissions.js
+++ app-permissions.js
@@ -4,3 +4,19 @@ const available = {
     launch: 'https://poddit.app',
   }
 };
+
+class AppInstaller {
+  constructor({ fetch, webId }) {
+    this.fetch = fetch;
+    this.webId = webId;
+  }
+  async getTypeIndexes() {
+    const res = await this.fetch(this.webId, {
+      // 'headers': {
+      //   'Accept': 'application/ld+json'
+      // }
+    });
+    const links = await res.json();
+    console.log(links);
+  }
+}
